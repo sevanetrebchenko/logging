7dc30762ef2f067604e572f8bbeb9abd31c85dbb
Seva Netrebchenko <seva.netrebchenko@digipen.edu>
2024-01-21 23:21:35 -0800

    WIP: Python-style format strings, redefining logging public interface.

--- include/logging/logging.hpp
+++ include/logging/logging.hpp
@@ -3,47 +3,56 @@
 #define LOGGING_LOGGING_HPP
 
 #include "logging/message.hpp"
+#include "logging/level.hpp"
 #include <string> // std::string
 #include <source_location> // std::source_location
+#include <cstdint> // std::uint16_t
+#include <vector> // std::vector
+#include <tuple> // std::tuple
 
 namespace logging {
-    
-    void init();
-    void shutdown();
-
     namespace internal {
-        
+
+		struct FormatSpecifier {
+			std::uint16_t offset; // Offset
+			std::string_view specifier;
+		};
+
         struct Log {
             public:
-                Log() {}
-                ~Log() {}
+				// Formats positional arguments.
+				template <typename ...Ps>
+                Log(MessageLevel level, const std::string& format, std::tuple<Ps...> positional_arguments, std::source_location source);
+                ~Log();
+
+				// Formats named arguments.
+				template <typename ...Ns>
+				void format(const Ns&... named_arguments) const;
             
-            protected:
-                template <typename T>
-                void parse();
-                
-                const char* m_format;
+            private:
+				MessageLevel m_level;
+                std::string m_format;
                 std::source_location m_source;
-                
+				std::vector<FormatSpecifier> m_specifiers;
         };
+
     }
+
+	void initialize();
+	void shutdown();
 
     // Note for all logging functions:
     // Constructor automatically submits the log message for output if the message format string does not contain named arguments.
     // Otherwise, log submit is done by calling format(...) and providing the appropriate named arguments.
     
-    template <typename ...Ts>
-    struct info : internal::Log {
-        info(const char* format, const Ts&..., std::source_location source = std::source_location::current());
-        ~info() {}
-        
-        template <typename ...Args>
-        void format(const Args&... args) const {}
+    template <typename ...Ps>
+    struct info final : internal::Log<Ps...> {
+        explicit info(std::string format, const Ps&... positional_arguments, std::source_location source = std::source_location::current());
     };
     template <typename ...Ts>
-    info(const char* format, const Ts&...) -> info<Ts...>;
-    
-//    void debug(Message message, ...);
+    info(std::string, const Ts&...) -> info<Ts...>;
+
+ //    void debug(Message message, ...);
 //    void warning(Message message, ...);
 //    void error(Message message, ...);
     
--- src/logging.cpp
+++ src/logging.cpp
@@ -3,6 +3,15 @@
 
 namespace logging {
 
+	namespace internal {
 
+		Log::Log(const char* format, std::source_location source) : m_format(format),
+																	m_source(source)
+																	{
+		}
+
+		Log::~Log() = default;
+
+	}
     
 }
